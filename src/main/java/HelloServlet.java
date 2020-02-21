6d8df7a002e2b07609885d33901dbc8860acbe40
Dinar.Timerzyanov <2>
2020-02-21 10:07:14 +0300

    first commit

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Dinar.Timerzyanov on 20.02.2020.
+ */
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Java HelloServlet Response");
+    }
+}
